97d665a419a54f1129b88f5c7c1565df3aa64617
Priyam Bajpai <115792556+itspriyam@users.noreply.github.com>
2023-06-26 12:43:32 +0530

    project task

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import React from "react";
 import "./App.css";
-// import KanbanBoard from "./KanbanBoard";
+import KanbanBoard from "./Components/KanbanBoard";
+import SidePanel from "./Components/SidePanel";
+import Header from "./Components/Header";
 
 
 const App = () => {
   return (
     <div className="app">
-      {/* <KanbanBoard/> */}
-      Your app content goes here
+      <Header/>
+      <KanbanBoard/>
+      <SidePanel/>
     </div>
   );
 };
 
 export default App;
+
+
